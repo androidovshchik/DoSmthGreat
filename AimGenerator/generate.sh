3f80d3d6ab6758142fe9fe970b85f737fc88b742
vlad <gimrcpp@gmail.com>
2018-05-10 21:36:47 +0300

    update generator

--- AimGenerator/generate.sh
+++ AimGenerator/generate.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "You must enter the new app name as 1-st argument"
+if [ "$#" -ge 3 ]; then
+    appName=$1From$2To$3
+elif [ "$#" -ge 2 ]; then
+    appName=$1From$2
+elif [ "$#" -ge 1 ]; then
+    appName=$1
+elif [ "$#" -ne 1 ]; then
+    echo You must enter at least folder name as the 1-st argument
     exit
 fi
 apks=`ls ./*.apk`
 for apk in $apks
 do
+   mkdir -p "$1"
    echo \> Found base apk \"$apk\"
-   package=rf.androidovshchik.do`date | md5sum | cut -c1-18`
-   echo \> It\'s new package name is $package
-   echo \> It\'s new app name is \"$1\"
+   packageName=rf.androidovshchik.do`date | md5sum | cut -c1-18`
+   echo \> New package name is $packageName
+   echo \> New app name is \"$appName\"
    echo \> Decompiling base apk
    java -jar ./tools/apktool_2.3.3.jar d -s -f -o ./temp $apk 2>/dev/null
-   sed -i "s/renameManifestPackage: null/renameManifestPackage: $package/g" ./temp/apktool.yml
-   sed -i "s/@string\/app_name/$1/g" ./temp/AndroidManifest.xml
-   echo \> Clearing renamed folder
-   rm -rf ./renamed/*
+   sed -i "s/renameManifestPackage: null/renameManifestPackage: $packageName/g" ./temp/apktool.yml
+   sed -i "s/@string\/app_name/$appName/g" ./temp/AndroidManifest.xml
    echo \> Building new apk
-   java -jar ./tools/apktool_2.3.3.jar b -o ./renamed/temp.apk ./temp 2>/dev/null
+   java -jar ./tools/apktool_2.3.3.jar b -o "./$1/temp.apk" ./temp 2>/dev/null
    echo \> Signing new apk
-   java -jar ./tools/uber-apk-signer-0.8.4.jar -a ./renamed --ks ./certificate.jks \
-       --ksAlias DoAlias --ksPass DoPassword --ksKeyPass DoPassword >/dev/null
+   java -jar ./tools/uber-apk-signer-0.8.4.jar -a "./$1" --ks ./certificate.jks --ksAlias DoAlias \
+       --ksPass DoPassword --ksKeyPass DoPassword >/dev/null
    echo \> Removing extra files
-   mv ./renamed/temp-aligned-signed.apk "./renamed/$1.apk"
-   rm ./renamed/temp.apk
+   mv "./$1/temp-aligned-signed.apk" "./$1/_$appName.apk"
+   rm "./$1/temp.apk"
    rm -rf ./temp
    echo \> All is done
    exit
 done
+echo No base apk found
