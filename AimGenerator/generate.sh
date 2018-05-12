d5e5583be7d04dd2ce926dabce7a3f5ab0e82f55
vlad <gimrcpp@gmail.com>
2018-05-12 09:40:10 +0300

    fix generator

--- AimGenerator/generate.sh
+++ AimGenerator/generate.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 if [ "$#" -ge 3 ]; then
-    appName=$1From$2To$3
+    appName="$1From$2To$3"
 elif [ "$#" -ge 2 ]; then
-    appName=$1From$2
+    appName="$1From$2"
 elif [ "$#" -ge 1 ]; then
-    appName=$1
+    appName="$1"
 elif [ "$#" -ne 1 ]; then
     echo You must enter at least folder name as the 1-st argument
     exit
 fi
-apks=`ls ./*.apk`
+mkdir -p "$1"
+find "./$1" -name "*.apk" -type f -delete
+alias=`echo "$1" | sed "s/[^a-zA-Z0-9.]/d/g" | tr 'A-Z' 'a-z'`
+# package name must start with letter (not number e.g.)
+packageName=rf.androidovshchik.i$alias
+echo \> New package name is $packageName
+echo \> New app name is \"$appName\"
+echo \> New alias, store and key passwords is \"$alias\"
+echo \> Setting configuration
+cd ..
+cp -f app/buildCopyForAimGenerator.txt app/build.gradle
+sed -i "s/rf.androidovshchik.dosmthgreat/$packageName/g" app/build.gradle
+sed -i "s/DoSmthGreat/$appName/g" app/build.gradle
+echo \> Generating certificate
+rm -f app/certificate.jks
+keytool -genkey -noprompt -keystore app/certificate.jks -alias $alias -storepass $alias \
+    -keypass $alias -dname "c=RU" -keyalg RSA -keysize 2048 -validity 10000 2>/dev/null
+echo \> Building new apk
+./gradlew assembleRelease -Pandroid.injected.signing.store.password=$alias \
+    -Pandroid.injected.signing.key.alias=$alias \
+    -Pandroid.injected.signing.key.password=$alias
+rm -f app/certificate.jks
+echo \> Moving files
+cp -f app/buildCopyForAimGenerator.txt app/build.gradle
+mv "app/release/$appName-mapping.txt" "AimGenerator/$1"
+apks=`ls app/build/outputs/apk/release | grep "_$appName.*\.apk$"`
 for apk in $apks
 do
-   echo \> Preparing
-   mkdir -p "$1"
-   find "./$1" -name "*.apk" -type f -delete
-   echo \> Found base apk \"$apk\"
-   alias=`echo "$1" | sed "s/[^a-zA-Z0-9.]/d/g" | tr 'A-Z' 'a-z'`
-   packageName=rf.androidovshchik.i$alias
-   echo \> New package name is $packageName
-   echo \> New app name is \"$appName\"
-   echo \> New alias is \"$alias\"
-   echo \> Decompiling base apk
-   rm -rf ./temp
-   java -jar ./tools/apktool_2.3.3.jar d -s -f -o ./temp $apk 2>/dev/null
-   sed -i "s/renameManifestPackage: null/renameManifestPackage: $packageName/g" ./temp/apktool.yml
-   sed -i "s/@string\/app_name/$appName/g" ./temp/AndroidManifest.xml
-   echo \> Building new apk
-   java -jar ./tools/apktool_2.3.3.jar b -o "./$1/_$appName.apk" ./temp 2>/dev/null
-   echo \> Signing new apk
-   rm -f ./certificate.jks
-   keytool -genkey -noprompt -keystore ./certificate.jks -alias $alias -storepass $alias \
-       -keypass $alias -dname "c=RU" -keyalg RSA -keysize 2048 -validity 10000 2>/dev/null
-   java -jar ./tools/uber-apk-signer-0.8.4.jar -a "./$1" --allowResign --ks ./certificate.jks \
-       --ksAlias $alias --ksPass $alias --ksKeyPass $alias --overwrite >/dev/null
-   echo \> Removing extra files
-   rm -f ./certificate.jks
-   rm -rf ./temp
-   echo \> All is done
-   exit
+    mv "app/build/outputs/apk/release/$apk" "AimGenerator/$1"
+    echo \> All is done
+    exit
 done
-echo No base apk found
+echo No apk found
